dc132309e9f6f3ecf41a72ad3f31072b7b835c44
tawan <87tawanzinho@gmail.com>
2023-12-24 02:45:49 -0300

    training basic

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,43 @@
 class Pessoa(val anoNascimento: Int, var nome: String) {
 
+    var doc: String = "" // in class, we need a default value
+
+    constructor(anoNascimento: Int, nome: String, doc: String) : this(anoNascimento, nome) // the object
+    {
+    this.doc = doc
+        if(doc == "") {
+            println("set a doc pls")
+        }
 }
+    val eyes = "blue"
 
+    fun dormir () {
+    println("I'm Sleeping.. Zzzz")
+    }
+
+    fun acordar() {
+    println("I'm wake up now.")
+    }
+}
+class Animal (var especie: String) {
+    var speak: String = ""
+    init {
+    speak = if (especie == "dog") {
+        "au"
+    } else if (especie == "cat") {
+        "meown"
+    } else {
+        ""
+    }
+
+              }
+    fun speaker () {
+        println(speak)
+
+    }
+}
+class Vazia private constructor() // no access to her
 fun main() {
-    var pessoa: Pessoa = Pessoa(1996, "tawan")
-
-    pessoa.nome // access attributes
+    Animal("dog").speaker()
 }
 
